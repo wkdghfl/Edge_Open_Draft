5e24bd8462c95b96eb45c476ffd27a4853fd1b02
이창호 <wkdghfl@gmail.com>
2026-05-12 10:52:15 +0900

    패밀리 파일일때 도면열기 오류 수정, 기존 dft 문자 변경헤서 시스템 확장자로 수정

--- Edge Open Draft/Program.cs
+++ Edge Open Draft/Program.cs
@@ -91,12 +91,11 @@ namespace Edge_Open_Draft
                 if (IsEdgeStart())
                 {
                     EdApp = (Application)Marshal2.GetActiveObject("SolidEdge.Application");
+                    String TStr = String.Empty;
 
-                    String TStr = String.Empty;                   
-
+                    // 1. 선택된 객체 유무에 따라 원본 파일 경로 가져오기
                     if (EdApp.ActiveSelectSet.Count > 0)
                     {
-                        
                         if (EdApp.ActiveSelectSet.Item(1) is SolidEdgeAssembly.Occurrence)
                         {
                             TStr = EdApp.ActiveSelectSet.Item(1).OccurrenceFileName;
@@ -105,18 +104,24 @@ namespace Edge_Open_Draft
                         {
                             TStr = EdApp.ActiveSelectSet.Item(1).Object.OccurrenceFileName;
                         }
-
-                        
-
                     }
                     else
                     {
                         TStr = EdApp.ActiveDocument.FullName;
-
                     } 
-                   
-                    TStr   = TStr.Substring(0,TStr.Length - 3) + "dft" ;
 
+                    // 2. 패밀리 맴버 체크 후 정리
+                    if (TStr.Contains("!"))
+                    {
+                        // "!"를 기준으로 문자를 쪼갠 뒤, 첫 번째 배열([0], 즉 진짜 경로)만 취함
+                        TStr = TStr.Split('!')[0];
+                    }
+
+                    // C# 기본 클래스를 이용해 안전하게 .dft로 변경 (문자열 길이 계산 오류 방지)
+                    TStr = System.IO.Path.ChangeExtension(TStr, ".dft");
+
+                    
+                    // 3. 파일 존재 확인 후 도면 열기
                     if (File.Exists(TStr))
                     {
                         EdApp.Documents.Open(TStr);
